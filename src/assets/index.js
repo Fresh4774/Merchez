ca0bc820e45e50e7bfbc92fadc67010b5c54c2bd
Friction <106335966+Fresh4774@users.noreply.github.com>
2023-10-25 05:28:39 +0530

    update

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,7 @@
 import fileIcon from "./file.png";
 import swatch from "./swatch.png";
-import download from "./download.png";
 
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 
-export { fileIcon, swatch, download, logoShirt, stylishShirt };
+export { fileIcon, swatch, logoShirt, stylishShirt };
